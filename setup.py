3dc90fcd26ddf5b58358a91c2455f3263189a952
Eduardo Bastos <eduardostimamiglio@gmail.com>
2023-02-22 10:20:38 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Yolov5',
+      version='1.0',
+      description='Retail Intelligence Yolo version 5',
+      author='Ultralytics',
+      url='https://github.com/ultralytics/yolov5',
+    )
