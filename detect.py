956bc6de198541c94310d239500ea4c5af27ba87
AlessandroDalponte <adalponte0@gmail.com>
2023-03-07 09:47:51 -0300

    fix detect.py return

--- detect.py
+++ detect.py
@@ -266,7 +266,7 @@ class YoloDetection():
                             self.vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                         self.vid_writer[i].write(im0)
             if len(det):
-                result_list = det_list
+                result_list.append(det_list)
 
             # Print time (inference-only)
             LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{self.dt[1].dt * 1E3:.1f}ms")
